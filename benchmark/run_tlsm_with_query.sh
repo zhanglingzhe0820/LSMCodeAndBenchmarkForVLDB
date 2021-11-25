227dc9fd0a013763ac700ff5a5830b6351d8de1c
zhanglingzhe <surevil@foxmail.com>
2021-11-25 19:56:39 +0800

    fix

--- benchmark/run_tlsm_with_query.sh
+++ benchmark/run_tlsm_with_query.sh
@@ -1,9 +1,9 @@
- ./stop_server.sh
+./stop_server.sh
 sudo rm -rf ../iotdb-with_seperation_policy_no_deletion/apache-iotdb-0.12.1-SNAPSHOT-all-bin/data &&
 time=$(date "+%Y%m%d-%H%M%S")
 nohup ../iotdb-with_seperation_policy_no_deletion/apache-iotdb-0.12.1-SNAPSHOT-all-bin/sbin/start-server.sh > tlsm_$1_$2_$3_$4_server.log &
 sleep 5
 csv_path="mu-${2}-sigma-${3}-interval-${1}-num-1000000.csv"
-java -jar -Djava.ext.dirs=../iotdb-with_seperation_policy_no_deletion/apache-iotdb-0.12.1-SNAPSHOT-all-bin/lib pure_write.jar ${csv_path} $4 100
+java -jar -Djava.ext.dirs=../iotdb-with_seperation_policy_no_deletion/apache-iotdb-0.12.1-SNAPSHOT-all-bin/lib write_while_query.jar ${csv_path} $4 100
 ../iotdb-with_seperation_policy_no_deletion/apache-iotdb-0.12.1-SNAPSHOT-all-bin/sbin/start-cli.sh -e "flush"
 sleep 5
